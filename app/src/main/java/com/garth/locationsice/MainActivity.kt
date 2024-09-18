611a134b60cd0c6f969369d3a8b2415e5733e9ae
gartdp <garthdp4@gmail.com>
2024-09-18 11:31:36 +0200

    fixed stuff

--- app/src/main/java/com/garth/locationsice/MainActivity.kt
+++ app/src/main/java/com/garth/locationsice/MainActivity.kt
@@ -16,24 +16,24 @@ import android.Manifest
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
+import android.util.Log
 import java.io.IOError
 import java.io.IOException
 import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
     private lateinit var locationManager: LocationManager
     private lateinit var  tvOutput: TextView
     private lateinit var locationTextView: TextView
     private var locationPermissionCode = 1
+    private lateinit var currentLocation: Location
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.btnFind)
-
-        button.setOnClickListener {
-            getLocation()
-        }
+        //https://api.geoapify.com/v2/places?categories=commercial.supermarket&bias=proximity:-122.084,37.421998333333335&limit=20&apiKey=1d71a3ed16f4429a9bb828d8e1e04d8b
+        getLocation()
     }
     private fun getLocation(){
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -53,6 +53,7 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onLocationChanged(location: Location){
         tvOutput = findViewById(R.id.lblLocation)
+        Log.d("location", location.latitude.toString() + " " + location.longitude.toString())
         tvOutput.text = "Latitude: " + location.latitude + ", \nLongitude: " + location.longitude
         getAddressFromLocation(location)
     }
